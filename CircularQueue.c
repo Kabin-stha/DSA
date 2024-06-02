6350cef51ed7bbb364729c90a7f933e3612e1cad
Kabin-stha <www.kabinkabin321@gmail.com>
2024-06-02 22:23:19 +0545

    dsa ko hw

---
+++ CircularQueue.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+typedef struct
+{
+    int data[MAX];
+    int front, rear;
+} CircularQueue;
+
+void initializeQueue(CircularQueue *q)
+{
+    q->front = q->rear = -1;
+}
+
+int isFull(CircularQueue *q)
+{
+    return q->front == (q->rear + 1) % MAX;
+}
+
+int isEmpty(CircularQueue *q)
+{
+    return q->front == -1;
+}
+
+void enqueue(CircularQueue *q, int value)
+{
+    if (isFull(q))
+    {
+        printf("Queue is full. Cannot enqueue.\n");
+        return;
+    }
+    if (isEmpty(q))
+        q->front = 0;
+    q->rear = (q->rear + 1) % MAX;
+    q->data[q->rear] = value;
+    printf("%d enqueued to the queue.\n", value);
+}
+
+void dequeue(CircularQueue *q)
+{
+    if (isEmpty(q))
+    {
+        printf("Queue is empty. Cannot dequeue.\n");
+        return;
+    }
+    printf("%d dequeued from the queue.\n", q->data[q->front]);
+    if (q->front == q->rear)
+    {
+        q->front = q->rear = -1;
+    }
+    else
+    {
+        q->front = (q->front + 1) % MAX;
+    }
+}
+
+void traverse(CircularQueue *q)
+{
+    if (isEmpty(q))
+    {
+        printf("Queue is empty.\n");
+        return;
+    }
+    printf("Queue elements are: ");
+    for (int i = q->front;; i = (i + 1) % MAX)
+    {
+        printf("%d ", q->data[i]);
+        if (i == q->rear)
+            break;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    CircularQueue q;
+    initializeQueue(&q);
+    int choice, value;
+
+    while (1)
+    {
+        printf("\nQueue Operations:\n1. Enqueue\n2. Dequeue\n3. Traverse\n4. Exit\nEnter your choice: ");
+        if (scanf("%d", &choice) != 1)
+        {
+            while (getchar() != '\n')
+                ;
+            printf("Invalid input. Please enter a number.\n");
+            continue;
+        }
+
+        switch (choice)
+        {
+        case 1:
+            printf("Enter value to enqueue: ");
+            if (scanf("%d", &value) != 1)
+            {
+                while (getchar() != '\n')
+                    ;
+                printf("Invalid input. Please enter a number.\n");
+                continue;
+            }
+            enqueue(&q, value);
+            break;
+        case 2:
+            dequeue(&q);
+            break;
+        case 3:
+            traverse(&q);
+            break;
+        case 4:
+            printf("Exiting...\n");
+            exit(0);
+        default:
+            printf("Invalid choice. Please try again.\n");
+        }
+    }
+    return 0;
+}
---
+++ Postfixtoinfix.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX 100
+
+typedef struct
+{
+    char *items[MAX];
+    int top;
+} Stack;
+
+void push(Stack *stack, char *item)
+{
+    stack->items[++stack->top] = item;
+}
+
+char *pop(Stack *stack)
+{
+    return stack->items[stack->top--];
+}
+
+char *postfixToInfix(char *postfix)
+{
+    Stack stack = {.top = -1};
+    for (int i = 0; postfix[i]; i++)
+    {
+        if (isspace(postfix[i]))
+            continue;
+        char *infix = (char *)malloc(MAX * sizeof(char));
+        if (isalnum(postfix[i]))
+        {
+            sprintf(infix, "%c", postfix[i]);
+        }
+        else
+        {
+            char *op2 = pop(&stack);
+            char *op1 = pop(&stack);
+            sprintf(infix, "(%s%c%s)", op1, postfix[i], op2);
+            free(op1);
+            free(op2);
+        }
+        push(&stack, infix);
+    }
+    return pop(&stack);
+}
+
+int main()
+{
+    char postfix[MAX];
+    printf("Enter a postfix expression: ");
+    fgets(postfix, MAX, stdin);
+    postfix[strcspn(postfix, "\n")] = 0;
+    char *infix = postfixToInfix(postfix);
+    printf("Infix expression: %s\n", infix);
+    free(infix);
+    return 0;
+}
