8c1c9e0b589fbfac3c39f86ced2e26c840d8128f
Kabin-stha <www.kabinkabin321@gmail.com>
2024-06-14 20:10:15 +0545

    aryan daju vayo?

--- Mergesort.c
+++ Mergesort.c
@@ -76,18 +76,6 @@ int main()
     printf("Enter the elements:\n");
     for (int i = 0; i < n; i++)
     {
-    printf("\n");
-}
-
-int main()
-{
-    int n;
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-    int arr[n];
-    printf("Enter the elements:\n");
-    for (int i = 0; i < n; i++)
-    {
         scanf("%d", &arr[i]);
     }
 
